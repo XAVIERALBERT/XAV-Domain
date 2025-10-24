a9c187619cadce73d616c2e42095f991f1e2c6f9
XAVIERALBERT <xav.al.cruz@gmail.com>
2025-10-24 14:27:13 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//LinkedIn button
+document.getElementById('Linkedin').addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/xavier-angeles-cruz/');
+});
+
+//GitHub button
+document.getElementById('GitHub').addEventListener('click', () => {
+    window.open('https://github.com/XAVIERALBERT');
+});
+
+//Resume button
+document.getElementById('Resume').addEventListener('click',() => {
+    window.open('files/Xavier Angeles-Cruz.pdf');
+});
